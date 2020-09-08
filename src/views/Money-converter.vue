df85c421a28bc346fe2c02e65f63e6e93afc9f6d
SiarheiDrachou <9kudza@tut.by>
2020-09-08 18:38:45 +0300

    Changes: 1) Added studies projects from Syntax school. Wrotes in december 2019 -january 2020.

---
+++ src/views/Money-converter.vue
@@ -0,0 +1,118 @@
+<template>
+    <div>
+        <div class="dropdown m-5">
+            <button 
+                class="btn btn-secondary dropdown-toggle" 
+                type="button" 
+                id="dropdownMenuButton" 
+                data-toggle="dropdown" 
+                aria-haspopup="true" 
+                aria-expanded="false"
+                ref="btn" 
+                v-show="!spinner"
+            >
+                {{startValue}}
+            </button>
+
+            <div 
+                class="dropdown-menu p-0" 
+                ref="dropmenu" 
+                @click="buildThead($event)" 
+                v-show="!spinner"
+            >
+            </div>
+        </div>
+        
+        <div class="spinner-border text-danger mx-auto" role="status" v-if="spinner"></div>
+
+        <table class="table w-75 mx-auto" ref="table" v-show="!spinner">
+            <thead class="thead-dark w-50">
+                <tr>
+                <th class="col w-100" scope="col" ref="col"> 
+                    USD Exchange rate
+                </th>
+                <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody class="table-body w-75" ref="tbody"></tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                money : null,
+                moneys: null,
+                spinner: true,
+                startValue: null
+            }
+        },
+        methods: {
+            startSession() {
+                if(!localStorage.getItem('valuta')) {
+                    localStorage.setItem('valuta', 'USD');
+                    let elMenu = document.querySelector(`.${localStorage.getItem('valuta')}`);
+                    elMenu.classList.add('active');
+                } 
+                this.startValue = localStorage.getItem('valuta');
+                this.readMoney();
+            },
+            buildTbody(arrKeys, arrValues) {
+                this.$refs.btn.innerHTML = localStorage.getItem('valuta');
+                this.$refs.col.innerHTML = localStorage.getItem('valuta') + " Exchange rate";
+                this.$refs.tbody.innerHTML = '';
+                for(let i = 0; i < arrValues.length; i++) {
+                    let tbodyTextEl =  `<tr class="row-${i+1}">
+                                            <th scope="row"> ${arrValues[i]} </th>
+                                            <td>${arrKeys[i]}</td>
+                                        </tr>`;
+                    this.$refs.tbody.insertAdjacentHTML('beforeend', tbodyTextEl);
+                }
+                this.buildButton(arrValues);
+            },
+            async readMoney () {
+                try {
+                    this.money = await fetch(`https://api.exchangerate-api.com/v4/latest/${localStorage.getItem('valuta')}`);
+                    this.moneys = await this.money.json();
+                    let arrValues = Object.keys(this.moneys.rates);
+                    let arrKeys = Object.values(this.moneys.rates);
+
+                    this.buildTbody(arrKeys, arrValues);
+                }
+                catch {
+                    alert("ERROR!!!")
+                }
+                finally {
+                    this.spinner = false;
+                }
+            },
+            buildThead(event) {
+                if(event.target.textContent.length == 3) {
+                    localStorage.setItem('valuta', event.target.textContent);
+                    let elMenu = event.target;
+                    for(let i = 0; i < this.$refs.dropmenu.children.length; i++) {
+                        let linkEl = this.$refs.dropmenu.children[i];
+                        linkEl.classList.remove('active');
+                    }
+                    elMenu.classList.add('active');
+                    this.readMoney();
+                }
+            },
+            buildButton(arrValues) {
+                for(let i = 0; i < arrValues.length; i++) {
+                    let values = `<a class="${arrValues[i]} item-${i} dropdown-item" href="#">${arrValues[i]}</a>`
+                    this.$refs.dropmenu.insertAdjacentHTML('beforeend', values);
+                }
+            }
+        },
+        mounted() {
+            this.startSession();
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+</style>
---
+++ src/views/Picture-gallery.vue
@@ -0,0 +1,187 @@
+<template>
+    <div>
+        <Loader v-if="!display" />
+        
+        <div class="container" v-show="display">
+            <h1 class="center-align"> Image gallery </h1>
+
+            <div 
+                class="row" 
+                ref="cardImg"
+            >
+            </div>
+        </div>
+        
+        <ul class="pagination container center-align" ref="listEl" @click="nextPage($event)" v-show="display"></ul>
+    </div>
+</template>
+
+<script>
+    import M from 'materialize-css';
+    import Loader from '../components/Picture-gallery/Loader'
+
+    export default {
+        data: function() {
+            return {
+                downloadImg:null,
+                imgJSON: null,
+                currentPage: null,
+                newPage: null,
+                limit: 40,
+                display: false,
+                start: 0
+            }
+        },
+        components: {
+            Loader
+        },
+        methods: {
+            async readImage() {
+                if(this.start === 0) {
+                    try { 
+                        this.downloadImg = await fetch(`https://picsum.photos/v2/list?limit=${this.limit}`);
+                        this.imgJSON = await this.downloadImg.json();
+
+                        this.getImage();
+                    }
+                    catch {
+                        alert("So sorry, it's error...")
+                    }
+                    finally {
+                        this.display = true;
+                        this.start++;
+                    }
+                }
+            },
+            startSession() {
+                this.currentPage = +localStorage.getItem('page');
+    
+                if (!this.currentPage) {
+                    this.currentPage = 1;
+                    localStorage.setItem('page', this.currentPage);
+                }
+            },
+            nextPage(event) {
+                if(this.$refs.listEl.firstElementChild.firstElementChild.firstElementChild === event.target) {
+                    this.left(event);
+                }
+                else if(this.$refs.listEl.lastElementChild.firstElementChild.firstElementChild === event.target) {
+                    this.right(event);
+                }
+                else if(this.$refs.listEl === event.target ) {
+                    return ;
+                }
+                else {
+                    this.newPage = event.target.textContent;
+                    localStorage.setItem('page', this.newPage);
+                    this.disabled();
+                    this.getImage();
+                }
+            },
+            left(event) {
+                const leftChevron = event.target;
+                this.currentPage = +localStorage.getItem('page');
+
+                if (leftChevron === this.$refs.listEl.firstElementChild.firstElementChild.firstElementChild && this.currentPage > 1) {
+                    this.newPage = this.currentPage - 1;
+                    localStorage.setItem('page', this.newPage);
+                    this.getImage();
+                    this.disabled();
+                }
+            },
+            right(event) {
+                const rightChevron = event.target;
+                this.currentPage = +localStorage.getItem('page');
+
+                if (rightChevron === this.$refs.listEl.lastElementChild.firstElementChild.firstElementChild && this.currentPage < Math.ceil(this.limit / 10)) {
+                    this.newPage = this.currentPage + 1;
+                    localStorage.setItem('page', this.newPage);
+                    this.getImage();
+                    this.disabled();
+                }
+            },
+            disabled() {
+                if (this.newPage == 1) {
+                    this.$refs.listEl.firstElementChild.classList.add('disabled');
+                    this.$refs.listEl.firstElementChild.classList.remove('waves-effect');
+                    this.$refs.listEl.lastElementChild.classList.remove('disabled');
+                    this.$refs.listEl.lastElementChild.classList.add('waves-effect');
+                }
+                else if (this.newPage == Math.ceil(this.limit / 10)) {
+                    this.$refs.listEl.lastElementChild.classList.add('disabled');
+                    this.$refs.listEl.lastElementChild.classList.remove('waves-effect');
+                    this.$refs.listEl.firstElementChild.classList.remove('disabled');
+                    this.$refs.listEl.firstElementChild.classList.add('waves-effect');
+                }
+                else {
+                    this.$refs.listEl.firstElementChild.classList.remove('disabled');
+                    this.$refs.listEl.lastElementChild.classList.remove('disabled');
+                    this.$refs.listEl.firstElementChild.classList.add('waves-effect');
+                    this.$refs.listEl.lastElementChild.classList.add('waves-effect');
+                }
+            },
+            paginationList() {
+                this.$refs.listEl.innerHTML = '';
+
+                let paginationItem = `  <li 
+                                            class="left waves-effect arrowL" 
+                                            ref="left"
+                                        >
+                                            <a href="#!"><i class="material-icons">chevron_left</i></a>
+                                        </li>
+                `
+                this.$refs.listEl.insertAdjacentHTML('afterbegin', paginationItem);
+                for (let i = 0; i < Math.ceil(this.limit / 10); i++) {
+                    paginationItem = `  <li 
+                                            class="waves-effect"
+                                        >
+                                            <a href="#!">${i+1}</a>
+                                        </li>`;
+                    this.$refs.listEl.insertAdjacentHTML('beforeend', paginationItem);
+
+                    if(this.limit <= 10) {
+                        this.newPage = 1;
+                        this.disabled();
+                    }
+                }
+
+                paginationItem = `  <li 
+                                        class="right waves-effect arrowR" 
+                                        ref="right"
+                                    >
+                                        <a href="#!"><i class="material-icons">chevron_right</i></a>
+                                    </li>
+                `
+                this.$refs.listEl.insertAdjacentHTML('beforeend', paginationItem);
+            },
+            getImage() {
+                this.$refs.cardImg.innerHTML = '';
+
+                for(let i = (+localStorage.getItem('page') - 1) * 10; i < +localStorage.getItem('page') * 10; i++) {
+                    let cardImage = `<img class="col l4 m6 s12 materialboxed" src="${this.imgJSON[i].download_url}">`;
+                    this.$refs.cardImg.insertAdjacentHTML('beforeend', cardImage);
+                }
+                M.AutoInit();
+            }
+        },
+        mounted() {
+            this.startSession();
+            this.readImage();
+            this.paginationList();
+            this.disabled();
+            M.AutoInit();
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import '../../node_modules/materialize-css/dist/css/materialize.min.css';
+
+    .preloader-wrapper {
+        display: block;
+    }
+
+    .material-placeholder {
+        display: inline !important;
+    }
+</style>
