2d701b5f8e3e99bbb43dcb70fe9d999dda68daed
SiarheiDrachou <9kudza@tut.by>
2020-09-12 22:53:31 +0300

    Modified AsideTagsSection.vue

--- src/components/car-blog/AsideTagsSection.vue
+++ src/components/car-blog/AsideTagsSection.vue
@@ -3,60 +3,78 @@
         <h3 class="tags-header"> Tags </h3>
 
         <div class="tags-block">
-            <span class="tags-text--Business tags-text"> 
-                Business 
-            </span>
-            <span class="tags-text--Freelance tags-text"> 
-                Freelance 
-            </span>
-            <span class="tags-text--Money tags-text"> 
-                Money 
-            </span>
-            <span class="tags-text--Experience tags-text"> 
-                Experience 
-            </span>
-            <span class="tags-text--Lifestyle tags-text"> 
-                Lifestyle 
-            </span>
-            <span class="tags-text--SEO tags-text"> 
-                SEO 
-            </span>
-            <span class="tags-text--Wordpress tags-text"> 
-                Wordpress 
-            </span>
-            <span class="tags-text--Marketing tags-text"> 
-                Marketing 
-            </span>
-            <span class="tags-text--UX tags-text"> 
-                UX 
-            </span>
-            <span class="tags-text--Modern tags-text"> 
-                Modern 
-            </span>
-            <span class="tags-text--Success tags-text"> 
-                Success 
-            </span>
-            <span class="tags-text--Nature tags-text"> 
-                Nature 
-            </span>
-            <span class="tags-text--Productivity tags-text"> 
-                Productivity 
-            </span>
-            <span class="tags-text--Sales-page tags-text"> 
-                Sales page 
-            </span>
-            <span class="tags-text--Sell tags-text"> 
-                Sell 
-            </span>
-            <span class="tags-text--Teamwork tags-text"> 
-                Teamwork 
-            </span>
-            <span class="tags-text--Monetize tags-text"> 
-                Monetize 
+            <span 
+                class="tags-text" 
+                v-for="(tag, id) in tags" 
+                :key="id" 
+                v-text="tag.text"
+            >  
             </span>
         </div>           
     </section>
 </template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            tags: [
+                {
+                    text: 'Business'
+                },
+                {
+                    text: 'Freelance'
+                },
+                {
+                    text: 'Money'
+                },
+                {
+                    text: 'Experience'
+                },
+                {
+                    text: 'Lifestyle'
+                },
+                {
+                    text: 'SEO'
+                },
+                {
+                    text: 'Wordpress'
+                },
+                {
+                    text: 'Marketing'
+                },
+                {
+                    text: 'UX'
+                },
+                {
+                    text: 'Modern'
+                },
+                {
+                    text: 'Success'
+                },
+                {
+                    text: 'Nature'
+                },
+                {
+                    text: 'Productivity'
+                },
+                {
+                    text: 'Sales page'
+                },
+                {
+                    text: 'Sell'
+                },
+                {
+                    text: 'Teamwork'
+                },
+                {
+                    text: 'Monetize'
+                }
+            ]
+        }
+    }
+}
+</script>
 
 <style lang="scss" scoped>
     .aside-tags {
@@ -65,236 +83,23 @@
         margin-bottom: 60px;
 
         .tags-header {
-            width: 267px;
             line-height: 21px;
-            margin-top: 0;
             margin-bottom: 26px;
         }
 
         .tags-block {
             display: flex;
+            justify-content: space-between;
             flex-wrap: wrap;
             height: 277px;
             width: 269px;
 
-            .tags-text--Business {
+            .tags-text {
                 background-color: #EBEBEB;
-                display: flex;
-                justify-content: center;
-                align-items: center;
                 border-radius: 3px;
-                height: 30px;
-                width: 84px;
-                margin: 0 7px 10px 0;
+                padding: 5px 11px;
                 font-size: 14px;
-                cursor: pointer;
-            }
-
-            .tags-text--Freelance {
-                background-color: #EBEBEB;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                border-radius: 3px;
                 height: 30px;
-                width: 91px;
-                margin: 0 7px 10px 0;
-                font-size: 14px;
-                cursor: pointer;
-            }
-
-            .tags-text--Money {
-                background-color: #EBEBEB;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                border-radius: 3px;
-                height: 30px;
-                width: 72px;
-                margin: 0 7px 10px 0;
-                font-size: 14px;
-                cursor: pointer;
-            }
-
-            .tags-text--Experience {
-                background-color: #EBEBEB;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                border-radius: 3px;
-                height: 30px;
-                width: 100px;
-                margin: 0 7px 10px 0;
-                font-size: 14px;
-                cursor: pointer;
-            }
-
-            .tags-text--Lifestyle {
-                background-color: #EBEBEB;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                border-radius: 3px;
-                height: 30px;
-                width: 82px;
-                margin: 0 7px 10px 0;
-                font-size: 14px;
-                cursor: pointer;
-            }
-
-            .tags-text--SEO  {
-                background-color: #EBEBEB;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                border-radius: 3px;
-                height: 30px;
-                width: 58px;
-                margin: 0 7px 10px 0;
-                font-size: 14px;
-                cursor: pointer;
-            }
-
-            .tags-text--Wordpress {
-                background-color: #EBEBEB;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                border-radius: 3px;
-                height: 30px;
-                width: 98px;
-                margin: 0 7px 10px 0;
-                font-size: 14px;
-                cursor: pointer;
-            }
-
-            .tags-text--Marketing {
-                background-color: #EBEBEB;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                border-radius: 3px;
-                height: 30px;
-                width: 93px;
-                margin: 0 7px 10px 0;
-                font-size: 14px;
-                cursor: pointer;
-            }
-
-            .tags-text--UX {
-                background-color: #EBEBEB;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                border-radius: 3px;
-                height: 30px;
-                width: 49px;
-                margin: 0 7px 10px 0;
-                font-size: 14px;
-                cursor: pointer;
-            }
-
-            .tags-text--Modern {
-                background-color: #EBEBEB;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                border-radius: 3px;
-                height: 30px;
-                width: 79px;
-                margin: 0 7px 10px 0;
-                font-size: 14px;
-                cursor: pointer;
-            }
-
-            .tags-text--Success {
-                background-color: #EBEBEB;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                border-radius: 3px;
-                height: 30px;
-                width: 79px;
-                margin: 0 7px 10px 0;
-                font-size: 14px;
-                cursor: pointer;
-            }
-
-            .tags-text--Nature {
-                background-color: #EBEBEB;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                border-radius: 3px;
-                height: 30px;
-                width: 72px;
-                margin: 0 7px 10px 0;
-                font-size: 14px;
-                cursor: pointer;
-            }
-
-            .tags-text--Productivity {
-                background-color: #EBEBEB;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                border-radius: 3px;
-                height: 30px;
-                width: 104px;
-                margin: 0 7px 10px 0;
-                font-size: 14px;
-                cursor: pointer;
-            }
-
-            .tags-text--Sales-page {
-                background-color: #EBEBEB;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                border-radius: 3px;
-                height: 30px;
-                width: 97px;
-                margin: 0 7px 10px 0;
-                font-size: 14px;
-                cursor: pointer;
-            }
-
-            .tags-text--Sell {
-                background-color: #EBEBEB;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                border-radius: 3px;
-                height: 30px;
-                width: 53px;
-                margin: 0 7px 10px 0;
-                font-size: 14px;
-                cursor: pointer;
-            }
-
-            .tags-text--Teamwork {
-                background-color: #EBEBEB;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                border-radius: 3px;
-                height: 30px;
-                width: 95px;
-                margin: 0 7px 10px 0;
-                font-size: 14px;
-                cursor: pointer;
-            }
-
-            .tags-text--Monetize {
-                background-color: #EBEBEB;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                border-radius: 3px;
-                height: 30px;
-                width: 88px;
-                margin: 0 7px 10px 0;
-                font-size: 14px;
                 cursor: pointer;
             }
 
